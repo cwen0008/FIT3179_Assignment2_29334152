0a094429c613b37585a76a6f2229489522bdf669
cwen0008 <cwen0008@student.monash.edu>
2023-10-11 21:10:53 +1100

    4th chart

--- js/FIT3179_A2.js
+++ js/FIT3179_A2.js
@@ -10,6 +10,6 @@ var spec3 = "js/GDP_Top5.vg.json";
 vegaEmbed("#linechart", spec3, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
-var spec4 = "js/A2_4.vg.json";
-vegaEmbed("#g4", spec4).then(function(result) {
+var spec4 = "js/Life_exp.vg.json";
+vegaEmbed("#linewithpoint", spec4).then(function(result) {
 }).catch(console.error);
